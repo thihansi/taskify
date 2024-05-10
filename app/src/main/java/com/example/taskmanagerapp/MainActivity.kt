89dd53ed1f14b478d765dc637e4b685a951f6a6f
thihansi <thihansig@gmail.com>
2024-05-10 23:02:04 +0530

    view model

--- app/src/main/java/com/example/taskmanagerapp/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagerapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.taskmanagerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val count: TextView = findViewById(R.id.number)
+        val button: Button = findViewById(R.id.button)
+        var number = 0
+
+        button.setOnClickListener {
+
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/taskmanagerapp/MainActivityViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.taskmanagerapp
+
+class MainActivityViewModel {
+    var number = 0
+    fun addNumber(){
+        number++
+    }
+}
