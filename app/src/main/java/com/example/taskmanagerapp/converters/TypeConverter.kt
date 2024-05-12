7a8444bbbf8f1021e6999b083dca491c4cb2155e
thihansi <thihansig@gmail.com>
2024-05-12 21:24:50 +0530

    conerter

--- app/src/main/java/com/example/taskmanagerapp/converters/TypeConverter.kt
+++ app/src/main/java/com/example/taskmanagerapp/converters/TypeConverter.kt
@@ -5,13 +5,5 @@ import java.util.Date
 
 class TypeConverter {
 
-    @TypeConverter
-    fun fromTimestamp(value:Long): Date {
-        return Date(value)
-    }
 
-    @TypeConverter
-    fun dateToTimestamp(date:Date): Long {
-        return date.time
-    }
 }
