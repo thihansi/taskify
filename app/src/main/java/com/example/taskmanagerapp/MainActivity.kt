3df1d45025ee98def742cc35f3591e27cae4b31a
thihansi <thihansig@gmail.com>
2024-05-12 16:02:42 +0530

    main activity

--- app/src/main/java/com/example/taskmanagerapp/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagerapp/MainActivity.kt
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.example.taskmanagerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val mainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(mainBinding.root)
     }
 }
