c94ab24546de5132da1766ab1e484a3a5f205947
thihansi <thihansig@gmail.com>
2024-05-12 18:49:48 +0530

    main activity changes - add task start

--- app/src/main/java/com/example/taskmanagerapp/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagerapp/MainActivity.kt
@@ -3,14 +3,23 @@ package com.example.taskmanagerapp
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
 import android.widget.ImageView
 import com.example.taskmanagerapp.databinding.ActivityMainBinding
 import com.example.taskmanagerapp.utils.setupDialog
+import com.example.taskmanagerapp.utils.validateEditText
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import java.util.Date
+
 
 class MainActivity : AppCompatActivity() {
     private val mainBinding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
     private val addTaskDialog: Dialog by lazy {
         Dialog(this, R.style.DialogCustomTheme).apply {
             setupDialog(R.layout.add_task_dialog)
@@ -22,20 +31,67 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private val loadingDialog: Dialog by lazy {
+        Dialog(this, R.style.DialogCustomTheme).apply {
+            setupDialog(R.layout.loading_dialog)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(mainBinding.root)
 
         // Add task start
         val addCloseImg = addTaskDialog.findViewById<ImageView>(R.id.closeImg)
-        val updateCloseImg = updateTaskDialog.findViewById<ImageView>(R.id.closeImg)
-
         addCloseImg.setOnClickListener { addTaskDialog.dismiss() }
-        updateCloseImg.setOnClickListener { updateTaskDialog.dismiss() }
 
+        val addETTitle = addTaskDialog.findViewById<TextInputEditText>(R.id.edTaskTitle)
+        val addETTitleL = addTaskDialog.findViewById<TextInputLayout>(R.id.edTaskTitleL)
 
-        mainBinding.addTaskFABtn.setOnClickListener{
+        addETTitle.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(s: Editable) {
+                validateEditText(addETTitle, addETTitleL)
+            }
+
+        })
+
+        val addETDesc = addTaskDialog.findViewById<TextInputEditText>(R.id.edTaskDesc)
+        val addETDescL = addTaskDialog.findViewById<TextInputLayout>(R.id.edTaskDescL)
+
+        addETDesc.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(s: Editable) {
+                validateEditText(addETDesc, addETDescL)
+            }
+        })
+
+        mainBinding.addTaskFABtn.setOnClickListener {
+            //clearEditText(addETTitle, addETTitleL)
+            //clearEditText(addETDesc, addETDescL)
             addTaskDialog.show()
         }
+
+        val saveTaskBtn = addTaskDialog.findViewById<Button>(R.id.saveTaskBtn)
+        saveTaskBtn.setOnClickListener {
+            if (validateEditText(addETTitle, addETTitleL)
+                && validateEditText(addETDesc, addETDescL)
+            ) {
+/*
+                val newTask = Task(
+                    //UUID.randomUUID().toString(),
+                    addETTitle.text.toString().trim(),
+                    addETDesc.text.toString().trim(),
+                    Date()
+                ) */
+                //hideKeyBoard(it)
+                addTaskDialog.dismiss()
+                //taskViewModel.insertTask(newTask)
+            }
+        }
+        // Add task end
+
     }
 }
