0ef9ce9c0d022b158d43516e5dceacf2503dfff8
thihansi <thihansig@gmail.com>
2024-05-12 21:20:49 +0530

    util changes

--- app/src/main/java/com/example/taskmanagerapp/utils/util.kt
+++ app/src/main/java/com/example/taskmanagerapp/utils/util.kt
@@ -1,8 +1,12 @@
 package com.example.taskmanagerapp.utils
 
 import android.app.Dialog
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.Toast
 import com.google.android.material.textfield.TextInputLayout
 
 enum class Status{
@@ -17,6 +21,17 @@ enum class StatusResult{
     Deleted
 }
 
+fun Context.hideKeyBoard(view : View){
+    try {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
+    }catch (e: Exception){
+        e.printStackTrace()
+    }
+}
+fun Context.longToastShow(msg:String){
+    Toast.makeText(this,msg,Toast.LENGTH_LONG).show()
+}
 fun Dialog.setupDialog(layoutResId: Int){
     setContentView(layoutResId)
     window!!.setLayout(
@@ -25,6 +40,7 @@ fun Dialog.setupDialog(layoutResId: Int){
     )
     setCancelable(false)
 }
+
 
 fun validateEditText(editText: EditText, textTextInputLayout: TextInputLayout): Boolean {
     return when {
@@ -37,4 +53,9 @@ fun validateEditText(editText: EditText, textTextInputLayout: TextInputLayout): 
             true
         }
     }
+}
+
+fun clearEditText(editText: EditText, textTextInputLayout: TextInputLayout) {
+    editText.text = null
+    textTextInputLayout.error = null
 }
