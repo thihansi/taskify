dd206a41dcb8010045b4c073b6f1768696bc9330
thihansi <thihansig@gmail.com>
2024-05-17 10:47:43 +0530

    changes

--- app/src/main/java/com/example/taskmanagerapp/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagerapp/MainActivity.kt
@@ -1,27 +1,48 @@
 package com.example.taskmanagerapp
 
 import android.app.Dialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
-import com.example.taskmanagerapp.databinding.ActivityMainBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import androidx.room.Query
+import com.example.taskmanagerapp.adapters.TaskRVVBListAdapter
 import com.example.taskmanagerapp.models.Task
+import com.example.taskmanagerapp.utils.Status
+import com.example.taskmanagerapp.utils.StatusResult
+import com.example.taskmanagerapp.utils.StatusResult.Added
+import com.example.taskmanagerapp.utils.StatusResult.Deleted
+import com.example.taskmanagerapp.utils.StatusResult.Updated
 import com.example.taskmanagerapp.utils.clearEditText
 import com.example.taskmanagerapp.utils.hideKeyBoard
-import com.example.taskmanagerapp.utils.setupDialog
+import com.example.taskmanagerapp.utils.longToastShow
 import com.example.taskmanagerapp.utils.validateEditText
 import com.example.taskmanagerapp.viewmodels.TaskViewModel
+import com.example.taskmanagerapp.databinding.ActivityMainBinding
+import com.example.taskmanagerapp.utils.setupDialog
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
-
-
 
 class MainActivity : AppCompatActivity() {
     private val mainBinding: ActivityMainBinding by lazy {
@@ -139,5 +160,219 @@ class MainActivity : AppCompatActivity() {
 
         // Update Task End
 
+        isListMutableLiveData.observe(this){
+            if (it){
+                mainBinding.taskRV.layoutManager = LinearLayoutManager(
+                    this, LinearLayoutManager.VERTICAL,false
+                )
+                mainBinding.listOrGridImg.setImageResource(R.drawable.ic_view_module)
+            }else{
+                mainBinding.taskRV.layoutManager = StaggeredGridLayoutManager(
+                    2, LinearLayoutManager.VERTICAL
+                )
+                mainBinding.listOrGridImg.setImageResource(R.drawable.ic_view_list)
+            }
+        }
+
+        mainBinding.listOrGridImg.setOnClickListener {
+            isListMutableLiveData.postValue(!isListMutableLiveData.value!!)
+        }
+
+        val taskRVVBListAdapter = TaskRVVBListAdapter(isListMutableLiveData ) { type, position, task ->
+            if (type == "delete") {
+                taskViewModel
+                    // Deleted Task
+//                .deleteTask(task)
+                    .deleteTaskUsingId(task.id)
+
+                // Restore Deleted task
+                restoreDeletedTask(task)
+            } else if (type == "update") {
+                updateETTitle.setText(task.title)
+                updateETDesc.setText(task.description)
+                updateTaskBtn.setOnClickListener {
+                    if (validateEditText(updateETTitle, updateETTitleL)
+                        && validateEditText(updateETDesc, updateETDescL)
+                    ) {
+                        val updateTask = Task(
+                            task.id,
+                            updateETTitle.text.toString().trim(),
+                            updateETDesc.text.toString().trim(),
+//                           here i Date updated
+                            Date()
+                        )
+                        hideKeyBoard(it)
+                        updateTaskDialog.dismiss()
+                        taskViewModel
+                            .updateTask(updateTask)
+//                            .updateTaskPaticularField(
+//                                task.id,
+//                                updateETTitle.text.toString().trim(),
+//                                updateETDesc.text.toString().trim()
+//                            )
+                    }
+                }
+                updateTaskDialog.show()
+            }
+        }
+        mainBinding.taskRV.adapter = taskRVVBListAdapter
+        ViewCompat.setNestedScrollingEnabled(mainBinding.taskRV,false)
+        taskRVVBListAdapter.registerAdapterDataObserver(object :
+            RecyclerView.AdapterDataObserver() {
+            override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
+                super.onItemRangeInserted(positionStart, itemCount)
+//                mainBinding.taskRV.smoothScrollToPosition(positionStart)
+                mainBinding.nestedScrollView.smoothScrollTo(0,positionStart)
+            }
+        })
+        callGetTaskList(taskRVVBListAdapter)
+        callSortByLiveData()
+        statusCallback()
+
+        callSearch()
+
     }
+
+    private fun restoreDeletedTask(deletedTask : Task){
+        val snackBar = Snackbar.make(
+            mainBinding.root, "Deleted '${deletedTask.title}'",
+            Snackbar.LENGTH_LONG
+        )
+        snackBar.setAction("Undo"){
+            taskViewModel.insertTask(deletedTask)
+        }
+        snackBar.show()
+    }
+
+    private fun callSearch() {
+        mainBinding.edSearch.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(query: Editable) {
+                if (query.toString().isNotEmpty()){
+                    taskViewModel.searchTaskList(query.toString())
+                }else{
+                    callSortByLiveData()
+                }
+            }
+        })
+
+        mainBinding.edSearch.setOnEditorActionListener{ v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH){
+                hideKeyBoard(v)
+                return@setOnEditorActionListener true
+            }
+            false
+        }
+
+        callSortByDialog()
+    }
+    private fun callSortByLiveData(){
+        taskViewModel.sortByLiveData.observe(this){
+            taskViewModel.getTaskList(it.second,it.first)
+        }
+    }
+
+    private fun callSortByDialog() {
+        var checkedItem = 0   // 2 is default item set
+        val items = arrayOf("Title Ascending", "Title Descending","Date Ascending","Date Descending")
+
+        mainBinding.sortImg.setOnClickListener {
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Sort By")
+                .setPositiveButton("Ok") { _, _ ->
+                    when (checkedItem) {
+                        0 -> {
+                            taskViewModel.setSortBy(Pair("title",true))
+                        }
+                        1 -> {
+                            taskViewModel.setSortBy(Pair("title",false))
+                        }
+                        2 -> {
+                            taskViewModel.setSortBy(Pair("date",true))
+                        }
+                        else -> {
+                            taskViewModel.setSortBy(Pair("date",false))
+                        }
+                    }
+                }
+                .setSingleChoiceItems(items, checkedItem) { _, selectedItemIndex ->
+                    checkedItem = selectedItemIndex
+                }
+                .setCancelable(false)
+                .show()
+        }
+    }
+
+    private fun statusCallback() {
+        taskViewModel
+            .statusLiveData
+            .observe(this) {
+                when (it.status) {
+                    Status.LOADING -> {
+                        loadingDialog.show()
+                    }
+
+                    Status.SUCCESS -> {
+                        loadingDialog.dismiss()
+                        when (it.data as StatusResult) {
+                            Added -> {
+                                Log.d("StatusResult", "Added")
+                            }
+
+                            Deleted -> {
+                                Log.d("StatusResult", "Deleted")
+
+                            }
+
+                            Updated -> {
+                                Log.d("StatusResult", "Updated")
+
+                            }
+                        }
+                        it.message?.let { it1 -> longToastShow(it1) }
+                    }
+
+                    Status.ERROR -> {
+                        loadingDialog.dismiss()
+                        it.message?.let { it1 -> longToastShow(it1) }
+                    }
+                }
+            }
+    }
+
+    private fun callGetTaskList(taskRecyclerViewAdapter: TaskRVVBListAdapter) {
+
+        CoroutineScope(Dispatchers.Main).launch {
+            taskViewModel
+                .taskStateFlow
+                .collectLatest {
+                    Log.d("status", it.status.toString())
+
+                    when (it.status) {
+                        Status.LOADING -> {
+                            loadingDialog.show()
+                        }
+
+                        Status.SUCCESS -> {
+                            loadingDialog.dismiss()
+                            it.data?.collect { taskList ->
+                                taskRecyclerViewAdapter.submitList(taskList)
+                            }
+                        }
+
+                        Status.ERROR -> {
+                            loadingDialog.dismiss()
+                            it.message?.let { it1 -> longToastShow(it1) }
+                        }
+                    }
+
+                }
+        }
+
+    }
+
+
 }
